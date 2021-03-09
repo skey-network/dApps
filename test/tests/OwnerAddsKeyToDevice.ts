c00a8710002d96b1625cc783166232717b8add00
mtrx93 <mtrx93@gmail.com>
2021-03-09 17:01:14 +0100

    test if requested key works

--- test/tests/OwnerAddsKeyToDevice.ts
+++ test/tests/OwnerAddsKeyToDevice.ts
@@ -5,6 +5,8 @@ import { IInvokeScriptCall } from '@waves/waves-transactions/dist/transactions'
 import { keyPair } from '@waves/ts-lib-crypto'
 
 const ACTIVE="active"
+const CLOSE="close"
+const OPEN="open"
 
 const OwnerAddsKeyToDevice = (th:TestHelper)=>{
   describe('OwnerAddsKeyToDevice', ()=>{
@@ -71,6 +73,53 @@ const OwnerAddsKeyToDevice = (th:TestHelper)=>{
     
       it('key added', async ()=>{
         expect(await th.walletValueFor(th.Device,`key_${th.requestedDeviceKey}`)).to.eq(ACTIVE)
+      })
+    })
+
+    describe('key opens device', function(){
+      let openCount
+      before(async ()=>{
+        openCount = await th.dappValueFor(`device_counter_${th.Device.address}`)
+        await th.txSuccess(
+          Transactions.data(
+            {
+              data: [
+                {key: `device_${th.Device.address}`, value: CLOSE} // make sure device was closed
+              ],
+              chainId: th.chainId,
+              fee: 500000,
+            },
+            th.Dapp.seed
+          )
+        )
+      })
+      
+      const invoker = th.DevOwner
+      it('invoke', async ()=>{
+          await th.txSuccess(Transactions.invokeScript({
+            dApp: th.Dapp.address,
+            chainId: th.chainId,
+            call:{
+              function:"deviceAction",
+              args:[
+                { type: "string", value: th.requestedDeviceKey},
+                { type: "string", value: "open"}
+                // { type: "string", value: invoker.address},
+              ]
+            },
+            payment: [],
+            fee: 500000
+            
+        },invoker.seed))
+      })
+
+      // it('asset recharged', async()=>{
+      //   await th.balance.expectRise(invoker)
+      // })
+
+      it('device opened', async ()=>{
+        expect(await th.dappValueFor(`device_${th.Device.address}`)).to.eq(OPEN)
+        expect(await th.dappValueFor(`device_counter_${th.Device.address}`)).to.eq(openCount + 1)
       })
     })
   })
